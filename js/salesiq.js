4103571e9ca980eafb60d2c8aacb3444ea0e35e8
219003113 <42488276+219003113@users.noreply.github.com>
2019-10-17 12:21:38 +0530

    Create salesiq.js

---
+++ js/salesiq.js
@@ -0,0 +1,2 @@
+
+var $zoho=$zoho || {};$zoho.salesiq = $zoho.salesiq || {widgetcode:"11cb97c6e9dce69db5690a542724688ae8380fb604854f15264127d67d5b1fe2", values:{},ready:function(){}};var d=document;s=d.createElement("script");s.type="text/javascript";s.id="zsiqscript";s.defer=true;s.src="https://salesiq.localzoho.com/widget";t=d.getElementsByTagName("script")[0];t.parentNode.insertBefore(s,t);d.write("<div id='zsiqwidget'></div>");
