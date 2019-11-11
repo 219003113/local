80203b199847aded4eadb1ce02cc6089e4ab83e4
219003113 <42488276+219003113@users.noreply.github.com>
2019-11-11 14:33:35 +0530

    Create kissmetrics.js

---
+++ js/kissmetrics.js
@@ -0,0 +1,12 @@
+var _kmq = _kmq || [];
+var _kmk = _kmk || '40f8345a405c5b3fb727c7195b86daec15f2eb41';
+function _kms(u){
+  setTimeout(function(){
+    var d = document, f = d.getElementsByTagName('script')[0],
+    s = d.createElement('script');
+    s.type = 'text/javascript'; s.async = true; s.src = u;
+    f.parentNode.insertBefore(s, f);
+  }, 1);
+}
+_kms('//i.kissmetrics.com/i.js');
+_kms('//scripts.kissmetrics.com/' + _kmk + '.2.js');
