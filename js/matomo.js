3eb8ee58f753f857c9d01612a4c8864e84ef51e7
219003113 <42488276+219003113@users.noreply.github.com>
2019-11-11 16:29:04 +0530

    matomo3

--- js/matomo.js
+++ js/matomo.js
@@ -1,5 +1,4 @@
-<!-- Matomo -->
-<script type="text/javascript">
+
   var _paq = window._paq || [];
   /* tracker methods like "setCustomDimension" should be called before "trackPageView" */
   _paq.push(["setDocumentTitle", document.domain + "/" + document.title]);
@@ -14,6 +13,3 @@
     var d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0];
     g.type='text/javascript'; g.async=true; g.defer=true; g.src='//cdn.matomo.cloud/nikecorp.matomo.cloud/matomo.js'; s.parentNode.insertBefore(g,s);
   })();
-</script>
-<noscript><p><img src="https://nikecorp.matomo.cloud/matomo.php?idsite=3&amp;rec=1" style="border:0;" alt="" /></p></noscript>
-<!-- End Matomo Code -->
