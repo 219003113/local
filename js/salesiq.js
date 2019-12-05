8f0518608efc22a16ff31a16613ff3ff10059508
219003113 <42488276+219003113@users.noreply.github.com>
2019-12-05 12:00:17 +0530

    Update salesiq.js

--- js/salesiq.js
+++ js/salesiq.js
@@ -1,2 +1,10 @@
 
 var $zoho=$zoho || {};$zoho.salesiq = $zoho.salesiq || {widgetcode:"11cb97c6e9dce69db5690a542724688ae8380fb604854f15264127d67d5b1fe2", values:{},ready:function(){}};var d=document;s=d.createElement("script");s.type="text/javascript";s.id="zsiqscript";s.defer=true;s.src="https://salesiq.localzoho.com/widget";t=d.getElementsByTagName("script")[0];t.parentNode.insertBefore(s,t);d.write("<div id='zsiqwidget'></div>");
+$zoho.salesiq.ready = function (embedinfo) {
+	    		$zoho.salesiq.visitor.trigger(function (triggername, visitorinfo) {
+			// Your code here
+		   	 if(triggername=='jsapi'){
+			    console.log("nice JSAPI");
+		    	}
+	   	 });
+		}
